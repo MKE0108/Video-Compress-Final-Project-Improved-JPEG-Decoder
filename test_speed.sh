75128bb0c7b791ce26b4c3923848040d98416bbb
MKE0108 <mickyt711ss@gmail.com>
2025-12-08 17:14:39 +0800

    update test.sh

--- test_speed.sh
+++ test_speed.sh
@@ -2,21 +2,21 @@
 
 start_time=$(date +%s%N)
 #without output in terminal
-taskset -c 0 time ./jpeg_decoder/build/jpeg_decoder test.jpg > /dev/null
+taskset -c 0 ./jpeg_decoder/build/jpeg_decoder test.jpg > /dev/null
 end_time=$(date +%s%N)
 elapsed=$((end_time - start_time))
-echo "Elapsed time: $((elapsed / 1000000)) ms"
+echo "Elapsed time: $((elapsed / 1000000)) ms(improved)"
 
 
 start_time=$(date +%s%N)
-taskset -c 0 time ./jpeg_decoder_old/build/jpeg_decoder test.jpg > /dev/null
+taskset -c 0 ./jpeg_decoder_old/build/jpeg_decoder test.jpg > /dev/null
 end_time=$(date +%s%N)
 elapsed=$((end_time - start_time))
-echo "Elapsed time: $((elapsed / 1000000)) ms"
+echo "Elapsed time: $((elapsed / 1000000)) ms(old)"
 
 #ffmpeg -vframes 1 -i test.jpg /dev/null
 start_time=$(date +%s%N)
-taskset -c 0 time ffmpeg -vframes 1 -i test.jpg -f bmp test.bmp > /dev/null 2>&1
+taskset -c 0 ffmpeg -vframes 1 -i test.jpg -f bmp test.bmp > /dev/null 2>&1
 end_time=$(date +%s%N)
 elapsed=$((end_time - start_time))
-echo "Elapsed time: $((elapsed / 1000000)) ms"
+echo "Elapsed time: $((elapsed / 1000000)) ms(ffmpeg)"
